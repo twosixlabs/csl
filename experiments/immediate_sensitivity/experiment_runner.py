f7351e1a42d3c47117968b396bfab54e82e20062
Tim Stevens <timstvns3@gmail.com>
2021-02-27 17:53:11 -0500

    baseline runner

--- experiments/immediate_sensitivity/experiment_runner.py
+++ experiments/immediate_sensitivity/experiment_runner.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from collections import defaultdict
 import numpy as np
+from autograd_hacks import *
 
 
 def loader_accuracy(model, test_loader, lf=nn.NLLLoss()):
@@ -96,6 +97,73 @@ def run_experiment(model, train_set, test_set, epsilon=1, alpha=25, epochs=10, a
 
             if plz_update:
                 model_optimizer.step()
+
+
+        
+        avg_test_acc, avg_test_l = loader_accuracy(model, test_loader, lf=model_criterion)
+        avg_train_l = sum(train_losses)/len(train_losses)
+            
+        tpr = mi.run_yeom_loader(model, avg_train_l, train_loader, lf=lf)
+        fpr = mi.run_yeom_loader(model, avg_train_l, test_loader, lf=lf)
+        adv = tpr-fpr
+
+        info['train_l'].append(avg_train_l.item())
+        info['test_l'].append(avg_test_l.item())
+        info['adv'].append(adv)
+        info['acc'] = avg_test_acc
+        info['merlin_adv'].append(mi.merlin_optimal_thresh(model, train_loader, test_loader, lf=lf, num_batches=20))
+        
+        if epoch % print_rate == 0:
+            acc = avg_test_acc
+            print(f'Epoch {epoch}: train loss {avg_train_l}, test loss {avg_test_l}, adv {adv}, acc {acc}')
+
+    return info, model
+
+
+def baseline_experiment(model, train_set, test_set, epsilon=1, alpha=25, C=2 epochs=10, add_noise=False, batch_size=32, lf=nn.NLLLoss, print_rate=1):
+    if epsilon==0:
+        add_noise=False
+    # reset the model
+    model.to(torch.cuda.current_device())
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, drop_last=True)
+    model_criterion = lf() 
+    model_optimizer = optim.Adam(model.parameters(),lr=0.001)
+    autograd_acks.add_hooks(model)
+    
+    epsilon_iter = epsilon / epochs
+
+    info = defaultdict(list)
+    train_accs = []
+    test_accs = []
+    advs = []
+    
+    train_losses = []
+    
+    for epoch in range(epochs):
+        for x_batch_train, y_batch_train in train_loader:
+            train_losses = []
+
+            model_optimizer.zero_grad()
+            inp = Variable(x_batch_train, requires_grad=True)
+            inp = inp.to(torch.cuda.current_device())
+            outputs = model.forward(inp)
+            y_batch_train = y_batch_train.to(torch.cuda.current_device())
+            loss = model_criterion(outputs, y_batch_train)
+            loss.backward()
+            autograd_hacks.compute_grad1(model)
+            clipper, mn = clipped_autograd(model, C)
+            max_norms.append(mn)
+            autograd_hacks.clear_backprops(model)
+            train_losses.append(loss) 
+            
+            if add_noise:
+                sigma = np.sqrt((C**2 * alpha) / (2 * epsilon_iter))
+                with torch.no_grad():
+                    for p in model.parameters():
+                        p.grad += (sigma * torch.randn(1,device=torch.cuda.current_device()).float())
+
+            model_optimizer.step()
 
 
         
