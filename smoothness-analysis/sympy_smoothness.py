1b7f865140c2608095f07ba586b492ab40540dec
Joe Near <jnear@uvm.edu>
2021-10-18 15:58:47 -0400

    changes from the meeting with Tim

--- smoothness-analysis/sympy_smoothness.py
+++ smoothness-analysis/sympy_smoothness.py
@@ -1,11 +1,11 @@
 import time
-
+import sys
 import numpy as np
 import sympy as sym
 from functools import reduce
 
 def L1_norm(terms):
-    return reduce(lambda x, y: x + y, terms, 0)
+    return reduce(lambda x, y: x + y, map(lambda x: MyAbs(x), terms), 0)
 
 def L2_norm(terms):
     sq_terms = [t**2 for t in terms]
@@ -13,7 +13,7 @@ def L2_norm(terms):
     return sym.sqrt(summation)
 
 def dot_product(xs, ys):
-    return L1_norm([x * y for x, y in zip(xs, ys)])
+    return reduce(lambda x, y: x + y, [x * y for x, y in zip(xs, ys)], 0)
 
 def build_weights(layer_num, input_size, width, all_weights):
     weights = []
@@ -26,12 +26,21 @@ def build_weights(layer_num, input_size, width, all_weights):
 def run_layer(inputs, weights):
     return [dot_product(inputs, w_row) for w_row in weights]
 
+# TODO: this might be broken because it doesn't have an "eval" method
 class Relu(sym.Function):
     def fdiff(self, x):
         if x > 0:
             return 1
         else:
             return 0
+
+# TODO: this might be broken because it doesn't have an "eval" method
+class MyAbs(sym.Function):
+    def fdiff(self, x):
+        if x > 0:
+            return 1
+        else:
+            return -1
 
 class L2Norm(sym.Function):
     def eval(x):
@@ -60,9 +69,9 @@ inputs = [sym.Symbol(f'x_{n}') for n in range(2)]
 # 3 layers, one hidden layer
 # generate the weights
 all_weights = []
-layer1_weights = build_weights(1, 3, 3, all_weights)
-layer2_weights = build_weights(2, 3, 3, all_weights)
-layer3_weights = build_weights(3, 3, 1, all_weights)
+layer1_weights = build_weights(1, 2, 1, all_weights)
+layer2_weights = build_weights(2, 1, 1, all_weights)
+layer3_weights = build_weights(3, 1, 1, all_weights)
 
 # run the network
 layer1_output = run_layer(inputs, layer1_weights)
@@ -73,14 +82,14 @@ layer3_output = run_layer(layer2_relu, layer3_weights)
 
 # calculate the loss
 label = sym.Symbol('y')
-pred_exp = L1_norm(layer3_output)
+pred_exp = reduce(lambda x, y: x + y, layer3_output, 0)
 loss = (pred_exp - label)**2
 
 # calculate immediate sensitivity
 inner_gradient = [sym.diff(loss, w) for w in all_weights]
-inner_norm = L2Norm(inner_gradient)
+inner_norm = L1_norm(inner_gradient)
 outer_gradient = [sym.diff(inner_norm, x) for x in inputs]
-outer_norm = L2Norm(outer_gradient)
+outer_norm = L1_norm(outer_gradient)
 immediate_sensitivity = outer_norm
 
 print('done constructing, time:', time.process_time() - start_time)
@@ -88,20 +97,30 @@ print('done constructing, time:', time.process_time() - start_time)
 print('substituting')
 start_time = time.process_time()
 
+subst = L1_norm([sym.diff(immediate_sensitivity, x) for x in inputs])
+
+print(f"def get_c({','.join([str(w) for w  in all_weights])}):")
+print(  'return ' + str(subst))
+
 # substitute in actual values for the weights
 weight_vals = [.1 for w in all_weights]
-subst = immediate_sensitivity
+#subst = immediate_sensitivity
 for w_name, w_val in zip(all_weights, weight_vals):
     subst = subst.subs(w_name, w_val)
 
 subst = subst.subs(label, 1)
 print('Immediate sensitivity:', subst)
+#sys.exit()
 #ivl = sym.Interval(-1,1)
+
 
 ggs_is = [sym.diff(subst, x) for x in inputs]
 print()
+print('GS of immediate sensitivity:', ggs_is)
 print('GS of immediate sensitivity:', L1_norm(ggs_is))
+print(inputs)
 
+print(weight_vals)
 
 exit()
 
